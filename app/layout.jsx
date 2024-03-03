9a86104744a256ffb7facbd9fb9d9b4d38249a03
Tony <Lahn26@gmail.com>
2024-03-03 09:53:39 -0500

    InfoBoxes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import '@/assets/styles/globals.css';
 
 export const metadata = {
-    title: 'DoorHub | Find the Perfect Rental',
+    title: 'DoorPulse | Find the Perfect Rental',
     description: 'Find your dream rental property',
     keywords: 'rental, find rentals, find properties',
 };
@@ -9,11 +11,13 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
-        <body>
-            <div>{ children }</div>
-        </body>
+      <body>
+        <Navbar></Navbar>
+        <main>{children}</main>
+        <Footer/>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default MainLayout
